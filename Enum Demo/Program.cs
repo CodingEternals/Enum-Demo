edbe87b3219711e382b3eba08d19fb1ae4fd3106
tanujbansal51@gmail.com <tanujbansal51@gmail.com>
2022-04-29 14:33:39 +0530

    Initial Commit

--- Enum Demo/Program.cs
+++ Enum Demo/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharpFuntamentals
+{
+    public enum ShipingMethod
+    {
+        regularAirMail=1,
+        registeredAirMail=2,
+        Express=3
+    }
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            ShipingMethod method = ShipingMethod.regularAirMail;
+            Console.WriteLine((int)method); //casting
+
+            var methodId = 2;
+            Console.WriteLine((ShipingMethod)methodId);
+
+            var methodName = "Express";
+
+            var shipmethod= (ShipingMethod) Enum.Parse(typeof(ShipingMethod), methodName);
+            Console.WriteLine(shipmethod);
+        }
+    }
+}
